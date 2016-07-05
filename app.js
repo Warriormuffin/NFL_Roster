dce04e7d6f89014350fa78a936c1a706706a8f2f
Nathan Elg <elgn@students.wwu.edu>
2016-07-05 10:14:54 -0600

    Trial and Error

---
+++ app.js
@@ -0,0 +1,49 @@
+var players = []
+
+
+function Player(name,position,jersey){
+    this.name = name;
+    this.position = position;
+    this.jersey= jersey;
+}
+
+$('form-group').on('submit', function(e){
+  e.preventDefault();
+  var form = this;
+  var newPlayer = new Player(form.playerName.value, form.playerPosition.value, form.playerJersey.value)
+  players.push(newPlayer);
+  
+  update(players);
+})
+
+function update(list){
+  var usersElem = $('.player-roster');
+  usersElem.empty()
+  for(var i = 0; i < list.length; i++){
+    usersElem.prepend('<div class="player-card"><h3>'+list[i].name+'<br>'+list[i].position+'<br>'+list[i].jersey+'</h3></div>' )  
+  }
+}
+function playerID(){
+  
+}
+$('.player-roster').on('click', '.player-card', function(){
+  var player = $(this).text()
+  console.log(this)
+  // removePlayerByCard(player);
+  // update(players)
+})
+ 
+function removePlayerByCard(name){
+  
+  for (var i = 0; i < players.length; i++) {
+    var currentPlayer = players[i];
+    var a =  (currentPlayer.name + currentPlayer.position + currentPlayer.jersey)
+    // console.log(name)
+    
+    if(a == name){
+      return players.splice(i,1)
+      
+      
+    }
+  } 
+}
